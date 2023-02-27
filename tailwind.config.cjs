2cd4141199a7c364a20470f01aa9d123d38dadd4
jurevito <jure.vito@gmail.com>
2023-02-27 12:26:42 +0200

    Add fonts to page

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,16 @@
+
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 const config = {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
 
 	theme: {
-		extend: {}
+		extend: {
+			fontFamily: {
+				opensans: ['"Open Sans"', ...defaultTheme.fontFamily.sans],
+				mont: ['"Montserrat"', ...defaultTheme.fontFamily.sans]
+			}
+		}
 	},
 
 	plugins: []
