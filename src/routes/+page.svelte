9686214badfc6a846b429f43b8bd6da59ab993bf
jurevito <jure.vito@gmail.com>
2023-03-19 21:31:24 +0200

    Add simple navigation bar

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -19,6 +19,19 @@
         mouse.y = event.clientY;
     }
 
+    function scrollToSection(sectionID: string) {
+
+        const section = document.getElementById(sectionID)!;
+        const sectionTop = section.offsetTop;
+
+        console.log("Button clicked: " + sectionTop);
+
+        window.scrollTo({
+            top: sectionTop,
+            behavior: 'smooth',
+        })
+    }
+
     onMount(() => {
         canvas.addEventListener('click', onClick, false);
 
@@ -130,11 +143,25 @@
     });
 </script>
 
+
+<nav class="bg-gray-800 py-3 px-4">
+    <div class="flex items-center justify-between">
+        <a href="/" class="text-white">Jure Vito Srovin</a>
+
+        <div class="flex items-center">
+            <button on:click={() => {scrollToSection('interests')}} class="text-gray-400 hover:text-white mx-4">Interests</button>
+            <button on:click={() => {scrollToSection('experiences')}} class="text-gray-400 hover:text-white mx-4">Experiences</button>
+            <button on:click={() => {scrollToSection('contact')}} class="text-gray-400 hover:text-white mx-4">Contact</button>
+        </div>
+    </div>
+</nav>
+
+<h2 id="interests" class="text-4xl text-center m-4 mt-8">Interests</h2>
 <div on:mousemove={onMouseMove} class="flex flex-row">
-    <div class="basis-1/2 ml-24 mr-2 my-6 p-4">
+    <div class="basis-1/2 ml-24 mr-2 my-2 p-3">
         <canvas bind:this={canvas} class="h-full w-full" id="canvas"/>
     </div>
-    <div class="basis-1/2 ml-2 mr-24 my-6 p-4">
+    <div class="basis-1/2 ml-2 mr-24 my-2 p-3">
         <div class="flex flex-col">
             <h2 class="font-mont text-2xl mb-1.5">Machine Learning & Deep Learning</h2>
             <div class="text-base">Why program business logic when you can let computer learn it himself. I used machine learning in many university assignments as well as in my bachelor's thesis with title: Machine Learning on Medical Data Using Interpretable Models.
@@ -142,3 +169,9 @@
         </div>
     </div>
 </div>
+
+<h2 id="experiences" class="text-4xl text-center m-4 mt-8">Experiences</h2>
+<p class="mx-16 my-4">Lorem ipsum</p>
+
+<h2 id="contact" class="text-4xl text-center m-4 mt-8">Contact</h2>
+<p class="mx-16 my-4">Lorem ipsum</p>
