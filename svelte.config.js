257b51d577f0fef4cc8e5c6a352a6e921ff34ad3
Jure Vito Srovin <jure.vito@gmail.com>
2023-04-09 23:12:38 +0200

    Add github pages adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,21 +1,22 @@
 import preprocess from 'svelte-preprocess';
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-github-pages';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
-	// for more information about preprocessors
-	preprocess: [
-		vitePreprocess(),
-		preprocess({
-			postcss: true
-		})
-	],
+    // Consult https://kit.svelte.dev/docs/integrations#preprocessors
+    // for more information about preprocessors
+    preprocess: [
+        vitePreprocess(),
+        preprocess({
+            postcss: true
+        })
+    ],
 
-	kit: {
-		adapter: adapter()
-	}
+    kit: {
+        adapter: adapter(),
+        target: '#svelte'
+    }
 };
 
 export default config;
