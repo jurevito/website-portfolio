000386441d074951bf5cce62070fc6ec279a7ef5
Jure Vito Srovin <jure.vit@gmail.com>
2023-04-26 14:03:11 +0300

    Updates

--- _app/immutable/chunks/1.1d3e2201.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../entry/error.svelte.c12b3299.js";export{t as component};
---
+++ _app/immutable/chunks/2.a1e83a6c.js
@@ -0,0 +1,1 @@
+import{default as t}from"../entry/_page.svelte.717b1758.js";export{t as component};
