0abdbc627d0bb10c2e8b051af0b134bb41f323e4
jurevito <jure.vito@gmail.com>
2023-05-20 00:05:03 +0300

    Sort experience by "from" field

--- src/lib/Experience.ts
+++ src/lib/Experience.ts
@@ -12,27 +12,29 @@ interface Experience {
     skills: string[],
 }
 
-const exp1: Experience = {
-    position: 'Software Engineer',
-    company: 'Globus Marine International',
-    type: 'Intership',
-    from: new Date('2021-04-01'),
-    until: new Date('2021-06-25'),
-    city: 'Ljubljana',
-    country: 'Slovenia',
-    skills: ['C#', '.NET', 'SQL Server'],
-};
+const exps: Experience[] = [
+    {
+        position: 'Software Engineer',
+        company: 'Globus Marine International',
+        type: 'Intership',
+        from: new Date('2021-04-01'),
+        until: new Date('2021-06-25'),
+        city: 'Ljubljana',
+        country: 'Slovenia',
+        skills: ['C#', '.NET', 'SQL Server'],
+    },
+    {
+        position: 'Data Scientist',
+        company: 'DikinBaws',
+        type: 'Full Time',
+        from: new Date('2022-02-01'),
+        until: 'Present',
+        city: 'Krakow',
+        country: 'Poland',
+        skills: ['Python', 'PyTorch', 'ScikitLearn'],
+    }
+];
 
-const exp2: Experience = {
-    position: 'Data Scientist',
-    company: 'DikinBaws',
-    type: 'Full Time',
-    from: new Date('2022-02-01'),
-    until: 'Present',
-    city: 'Krakow',
-    country: 'Poland',
-    skills: ['Python', 'PyTorch', 'ScikitLearn'],
-};
+exps.sort((a, b) => b.from.getTime() - a.from.getTime())
 
-const exps: Experience[] = [exp1, exp2];
 export { exps };
