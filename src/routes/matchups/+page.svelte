a2fd12a773b5684563a32813df5cf8dd65318ca0
Jure Vito Srovin <jure.vito@gmail.com>
2025-04-30 12:45:25 +0200

    style: adjust css style

--- src/routes/matchups/+page.svelte
+++ src/routes/matchups/+page.svelte
@@ -142,52 +142,54 @@
   }
 </script>
 
-<div class="mx-auto max-w-screen-2xl px-4 my-8 font-mont">
+<div class="mx-auto max-w-screen-2xl px-4 py-8 font-mont bg-gray-100 h-full w-full">
   <div class="grid grid-cols-2 gap-8">
-    <div class="rounded-md shadow p-6 overflow-y-auto max-h-screen">
+    <div class="rounded-md shadow p-6 overflow-y-auto max-h-[90vh] bg-white">
       {#if boxers.length == 0}
-        <div class="grid w-full max-w-sm items-center gap-1.5">
+        <div class="space-y-2">
           <Label for="csv-file">Upload CSV File</Label>
           <Input id="csv-file" type="file" accept=".csv" onchange={handleFileAdded} />
         </div>
       {:else}
-        <Button onclick={clearBoxers}>Clear Boxers</Button>
-        <div>
-          <Table.Root>
-            <Table.Caption>List of {boxers.length} registered boxers.</Table.Caption>
-            <Table.Header>
-              <Table.Row>
-                <Table.Head>Name</Table.Head>
-                <Table.Head>Club</Table.Head>
-                <Table.Head>Year</Table.Head>
-                <Table.Head>Weight</Table.Head>
-                <Table.Head>Num. Matches</Table.Head>
-              </Table.Row>
-            </Table.Header>
-            <Table.Body>
-              {#each boxers as boxer}
+        <div class="space-y-4">
+          <Button onclick={clearBoxers}>Clear Boxers</Button>
+          <div>
+            <Table.Root>
+              <Table.Caption>List of {boxers.length} registered boxers.</Table.Caption>
+              <Table.Header>
                 <Table.Row>
-                  <Table.Cell class="flex">
-                    {#if boxer.hasMatch}
-                      <Check class="mr-2 size-4 text-green-500" />
-                    {:else}
-                      <Cross class="mr-2 size-4 text-red-500" />
-                    {/if}
-                    {boxer.name}
-                  </Table.Cell>
-                  <Table.Cell>{boxer.club}</Table.Cell>
-                  <Table.Cell>{boxer.year}</Table.Cell>
-                  <Table.Cell>{boxer.weight}</Table.Cell>
-                  <Table.Cell>{boxer.fightCount}</Table.Cell>
+                  <Table.Head>Name</Table.Head>
+                  <Table.Head>Club</Table.Head>
+                  <Table.Head>Year</Table.Head>
+                  <Table.Head>Weight</Table.Head>
+                  <Table.Head># Matches</Table.Head>
                 </Table.Row>
-              {/each}
-            </Table.Body>
-          </Table.Root>
+              </Table.Header>
+              <Table.Body>
+                {#each boxers as boxer}
+                  <Table.Row>
+                    <Table.Cell class="flex">
+                      {#if boxer.hasMatch}
+                        <Check class="mr-2 size-4 text-green-500" />
+                      {:else}
+                        <Cross class="mr-2 size-4 text-red-500" />
+                      {/if}
+                      {boxer.name}
+                    </Table.Cell>
+                    <Table.Cell>{boxer.club}</Table.Cell>
+                    <Table.Cell>{boxer.year}</Table.Cell>
+                    <Table.Cell>{boxer.weight}</Table.Cell>
+                    <Table.Cell>{boxer.fightCount}</Table.Cell>
+                  </Table.Row>
+                {/each}
+              </Table.Body>
+            </Table.Root>
+          </div>
         </div>
       {/if}
     </div>
 
-    <div class="rounded-md shadow p-6 overflow-y-auto max-h-screen">
+    <div class="rounded-md shadow p-6 overflow-y-auto max-h-[90vh] bg-white">
       <div>
         <Button onclick={GetMatchups} disabled={optimizing} class="w-full sm:w-auto min-w-[12rem]">
           <Shuffle class="mr-2 size-4" />
